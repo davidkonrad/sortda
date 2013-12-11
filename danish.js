0d68256c4341933bfaf5041fd4dd9112f6a034bd
davidkonrad <davidkonrad@gmail.com>
2013-12-11 20:23:48 +0100

    Create danish.js

---
+++ danish.js
@@ -0,0 +1,49 @@
+/* 
+ * some real life danish locations / place names
+ * demonstrating the typically problems by sorting danish strings in javascript
+ *
+ */
+
+var strings=[
+	'Aakirkeby',
+	'København',
+	'Ålestrup',
+	'Ørestaden',
+	'Sorø',
+	'Hvidovre',
+	'Aalborg',
+	'Aarhus',
+	'Linné',
+	'Linned',
+	'Linette',
+	'Liseleje',
+	'Løgstør',
+	'Løgstrup',
+	'Haas',
+	'Hastrup',
+	'Hasselbo',
+	'Heerup',
+	'Halskov',
+	'Læsø',
+	'Lærred',
+	'Lærkedal',
+	'Løkken',
+	'Løgstrup',
+	'Løftgårde',
+	'Løftgaarde',
+	'Hågerup',
+	'Haarby Å',
+	'Sæby',
+	'Søllerød',
+	'Albæk',
+	'Almindingen',
+	'Almind',
+	'Alem Kirkegård',
+	'Å Højrup',
+	'Bådsgård Vig',
+	'Bådsgård Mark',
+	'Bagå',
+	'Badskær Kirke'
+];
+	
+	
