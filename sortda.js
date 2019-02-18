c7b5344c665817e07f6cc24972ff668b1cdd09ff
davidkonrad <davidkonrad@gmail.com>
2019-02-18 11:59:15 +0100

    fixed tabs

--- sortda.js
+++ sortda.js
@@ -2,11 +2,10 @@
  * javascript array sort with respect of danish characters
  * supports æ, ø, å and aa (double-a)
  *
- * Copyright 2013 David Konrad
+ * Copyright 2013- David Konrad
  * davidkonrad at gmail dot com
  *
  * Released under the MIT license
- * - and any other open source free to misuse license
  * 
  */
 
@@ -17,55 +16,56 @@ var AA_FIRST =	1,  // aa is sorted to the beginning of the array, before a
 
 Array.prototype.sortDa = function(aamode) {
 	this.sort(function(a,b) {
-	    var d, e, f;
-	    aamode = (typeof aamode == "undefined") ? AA_LAST : aamode;
-	    function intArray(c) {
-	        var array=[];
-	        c=c.toLowerCase();
-	        for (var i=0;i<c.length;i++) {
-	            if (c.substr(i,2)==='aa') {
-			switch (aamode) {
-			    case AA_FIRST : 
-				array.push(0);
-				break;
-			    case AA_ARING : 
-				array.push(299);
-				break;
-			    default :
-			        array.push(300); 
-				break;
+		var d, e, f;
+		aamode = (typeof aamode == "undefined") ? AA_LAST : aamode;
+
+		function intArray(c) {
+			var array=[];
+			c=c.toLowerCase();
+			for (var i=0; i<c.length; i++) {
+				if (c.substr(i,2) === 'aa') {
+					switch (aamode) {
+						case AA_FIRST : 
+							array.push(0);
+							break;
+						case AA_ARING : 
+							array.push(299);
+							break;
+						default :
+							array.push(300); 
+							break;
+					}
+					i=i+1;
+				} else {
+					switch (c.charCodeAt(i)) {
+						case 229 : //å
+							array.push(299);
+							break;
+						case 248 : //ø
+							array.push(298);
+							break;
+						case 230 : //æ
+							array.push(297);
+							break;
+						default : 
+							array.push(c.charCodeAt(i));
+							break;
+					}
+				}
 			}
-			i=i+1;
-	            } else {
-	                switch (c.charCodeAt(i)) {
-	                    case 229 : //å
-	                        array.push(299);
-	                        break;
-	                    case 248 : //ø
-	                        array.push(298);
-	                        break;
-	                    case 230 : //æ
-	                        array.push(297);
-	                        break;
-	                    default : 
-	                        array.push(c.charCodeAt(i));
-	                        break;
-	                }
-	            }
-	        }
-	        return array;
-	    }
-	    d=intArray(a), e=intArray(b);
-	    for (f=0;f<d.length;f++) {
-	        if (d[f]!=e[f]) {
-	        	if (f==e.length){
-	        		return 1; //d has more chars than e
-	        	}else{
-	           		 return d[f]-e[f];
-	            }
-	        }
-	    }
-	    return -1;//e has more chars than d 
+			return array;
+		}
+		d = intArray(a), e = intArray(b);
+		for (f=0;f<d.length;f++) {
+			if (d[f]!=e[f]) {
+				if (f==e.length){
+					return 1; //d has more chars than e
+				} else {
+					return d[f]-e[f];
+				}
+			}
+		}
+		return -1; //e has more chars than d 
 	});
 	return this;
 };
